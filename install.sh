9ea0f67bff859948af5cd3c9ead32fdcccc6f6f5
Grainier Perera <ivone.perera@gmail.com>
2015-02-03 21:09:31 +0530

    iptables flush added

--- install.sh
+++ install.sh
@@ -8,3 +8,8 @@ if it is 0 enable it
 sudo sysctl -w net.ipv4.ip_forward=1
 
 how to make it permanent [http://www.fclose.com/1372/setting-up-gateway-using-iptables-and-route-on-linux/]
+
+
+
+Add PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:/path/to/orion-ids
--- iptables.rules.sh
+++ iptables.rules.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Enable IPV4 Port Forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# Flush rules
+sudo iptables -F
+
+# Delete all chains
+sudo iptables -X
+
+# Add port Forwarding
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9191
 
 sudo iptables -t nat -L
