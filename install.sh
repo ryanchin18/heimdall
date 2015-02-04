151a463075db8dd4b4e99e8bf3a70e9d90444d2c
Grainier Perera <ivone.perera@gmail.com>
2015-02-04 11:31:13 +0530

    iptables problem resolved

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
+#!/bin/sh
 sudo apt-get install python-setuptools python-dev
 easy_install pip
 pip install -r requirements.txt
 
-check ip_forward is enabled
-cat /proc/sys/net/ipv4/ip_forward
-if it is 0 enable it
-sudo sysctl -w net.ipv4.ip_forward=1
-
-how to make it permanent [http://www.fclose.com/1372/setting-up-gateway-using-iptables-and-route-on-linux/]
-
-
-
-Add PYTHONPATH
+# Add PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:/path/to/orion-ids
--- iptables.rules.sh
+++ iptables.rules.sh
@@ -3,13 +3,21 @@
 # Enable IPV4 Port Forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
 
+# Activate it permanantly
+# sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+
+# Activate the changes
+sudo sysctl -p
+
 # Flush rules
 sudo iptables -F
+sudo iptables -t nat -F
 
 # Delete all chains
 sudo iptables -X
 
-# Add port Forwarding
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9191
+# PREROUTING isn't used by the loopback interface, you need to also add an OUTPUT rule:
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 9191
+sudo iptables -t nat -A OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 9191
 
 sudo iptables -t nat -L
