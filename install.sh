be105d7f6c0f1aca25587abc7045bb5511ca0bf7
Grainier Perera <ivone.perera@gmail.com>
2015-02-04 20:47:46 +0530

    install updated with graph-tools

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ pip install -r requirements.txt
 
 # Add PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:/path/to/orion-ids
+
+# Installing Graph Tools
+# Visit http://graph-tool.skewed.de/download#debian for more information
+deb http://downloads.skewed.de/apt/DISTRIBUTION DISTRIBUTION universe
+deb-src http://downloads.skewed.de/apt/DISTRIBUTION DISTRIBUTION universe
+sudo apt-get update
+sudo apt-get install python-graph-tool
