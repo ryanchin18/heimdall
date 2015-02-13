dc76892ad06d0f822a0a624ce2ef431084edcec8
Grainier Perera <ivone.perera@gmail.com>
2015-02-13 17:21:28 +0530

    initial stages of injecting referer to response content

--- interceptor/client_protocol.py
+++ interceptor/client_protocol.py
@@ -1,6 +1,8 @@
 """
 
 """
+import re
+import hashlib
 from twisted.internet import protocol
 from interceptor import parse_response
 
@@ -17,15 +19,17 @@ class ClientProtocol(protocol.Protocol):
 
     # Server => Proxy
     def dataReceived(self, data):
-        # Need to get response code / specially if there is an Error
         # Need to get time (If there is a way)
 
         # ------------------------------------------------------------
         # here we can extract http response content
         response = parse_response(data)
         content = response.read(len(data))
-        print("FROM SERVER")
-        print("Server IP : " + str(self.transport.getPeer()))
+        rq_uri = self.factory.server.request_uri
+        print "Request-URI", rq_uri
+        print "FROM SERVER"
+        print "Server IP : ", str(self.transport.getPeer())
+        print "Client IP : ", str(self.factory.server.transport.getPeer())
         print "status:", response.status
         print "Date:", response.getheader('Date')
         print "Server:", response.getheader('Server')
@@ -40,14 +44,40 @@ class ClientProtocol(protocol.Protocol):
         print "Content:", content
         # ------------------------------------------------------------
 
+        # ------------------------------------------------------------
         # Manipulate responses
-        # Only if it seems to be ref is disabled / or faked append 2 values.
-        #   1. md5 encoded referer as o_ref
-        #   2. verification o_ver
+        # Only if it seems to be ref is disabled / or faked
+        # append md5 encoded referer as md5_ref
+
         man_data = data
+        # man_data = unicode(data, 'utf8')
+
+        # strip get parameters
+        try:
+            rq_uri = rq_uri[:rq_uri.index('?')]
+            pass
+        except ValueError:
+            pass
+
+        # md5 the rq_uri
+        md5_ref = hashlib.md5(rq_uri).hexdigest()
+
+        # TODO : add to redis (as reference)
+
+        # TODO : check is resource???? if resource do not manipulate
+
+        # inject for href surround with " or ' or none of them ie : href=about.php
+        # ur'href=([\"|\'])([^"]+)([\"|\'])'
+        man_data = re.sub(ur'href(=\"|=\'|=)(.*?)(\"|\'|>| |/>)', (
+            lambda m:
+                'href{0}{1}&o_ref={3}{2}'.format(m.group(1), m.group(2), m.group(3), md5_ref) if '?' in m.group(2)
+                else 'href{0}{1}?o_ref={3}{2}'.format(m.group(1), m.group(2), m.group(3), md5_ref)
+        ), data)
+        # ------------------------------------------------------------
 
         # continue with the response
-        self.factory.server.write(data)
+        # self.factory.server.write(data)
+        self.factory.server.write(man_data)
         pass
 
     # Proxy => Server
